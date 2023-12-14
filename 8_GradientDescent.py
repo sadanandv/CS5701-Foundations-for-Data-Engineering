9885454746a836f985be2cad769053ed018696e1
Sadanand Venkataraman <60425347+sadanandv@users.noreply.github.com>
2023-12-13 21:53:54 -0800

    Add files via upload

--- 8_GradientDescent.py
+++ 8_GradientDescent.py
@@ -1,44 +1,93 @@
-'''8. Given  f(x,y)=3x 2 +2y 2 - 4x+6y+7 , find the minimum value of the function. Write Python code to find the minimum value of the function using gradient descent. Obtain contour plots of the derived function.'''
-  
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the function
-def f(x, y):
-    return 3*x**2 + 2*y**2 - 4*x + 6*y + 7
+# Function to minimize
+def function_to_minimize(x, y):
+    return x**2 + y**2
 
-# Define the gradient of the function
-def grad_f(x, y):
-    df_dx = 6*x - 4
-    df_dy = 4*y + 6
+# Gradient of the function
+def gradient(x, y):
+    df_dx = 2 * x
+    df_dy = 2 * y
     return np.array([df_dx, df_dy])
 
 # Gradient Descent
-def gradient_descent(start_x, start_y, learning_rate, num_iterations):
-    x, y = start_x, start_y
+def gradient_descent(learning_rate, num_iterations):
+    # Initial values
+    x = 5.0
+    y = 5.0
+
+    # Lists to store values for plotting
+    x_values = [x]
+    y_values = [y]
+    z_values = [function_to_minimize(x, y)]
+
+    # Gradient Descent iterations
     for _ in range(num_iterations):
-        grad_x, grad_y = grad_f(x, y)
-        x -= learning_rate * grad_x
-        y -= learning_rate * grad_y
-    return x, y
+        grad = gradient(x, y)
+        x -= learning_rate * grad[0]
+        y -= learning_rate * grad[1]
 
-# Perform Gradient Descent
-min_x, min_y = gradient_descent(start_x=0.5, start_y=0.5, learning_rate=0.1, num_iterations=100)
+        # Store values for plotting
+        x_values.append(x)
+        y_values.append(y)
+        z_values.append(function_to_minimize(x, y))
 
-# Print the minimum value
-print(f"Minimum at: ({min_x}, {min_y})")
-print(f"Minimum value of the function: {f(min_x, min_y)}")
+    return x_values, y_values, z_values
+
+# Surface plot
+def plot_surface():
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = function_to_minimize(X, Y)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z )#, cmap='viridis', alpha=0.8)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('F(x, y)')
+    ax.set_title('Surface Plot')
+
+    plt.show()
 
 # Contour plot
-x = np.linspace(-1, 1, 400)
-y = np.linspace(-1, 1, 400)
+def plot_contour():
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = function_to_minimize(X, Y)
+
+    plt.contour(X, Y, Z)# levels=20, cmap='viridis')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Contour Plot')
+    plt.show()
+
+# Gradient Descent parameters
+learning_rate = 0.1
+num_iterations = 50
+
+# Perform Gradient Descent
+x_values, y_values, z_values = gradient_descent(learning_rate, num_iterations)
+
+# Plot results
+plot_surface()
+plot_contour()
+
+# Plotting the path of gradient descent on the contour plot
+x = np.linspace(-10, 10, 100)
+y = np.linspace(-10, 10, 100)
 X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
+Z = function_to_minimize(X, Y)
 
-plt.contour(X, Y, Z, levels=50)
-plt.plot(min_x, min_y, 'r*', markersize=10)
-plt.title('Contour plot of and Gradient Descent Path')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.contour(X, Y, Z, levels=20, cmap='viridis')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Contour Plot')
+plt.plot(x_values, y_values, 'ro-')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Contour Plot with Gradient Descent Path')
 plt.show()
-
